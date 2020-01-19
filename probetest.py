307e5de76d30d56a401c36297d8aa64b7aad4075
EmperorOfPau <carlos@mt2015.com>
2020-01-19 23:02:19 +0100

    how the fuck do I save this shit? xD

--- probetest.py
+++ probetest.py
@@ -2,13 +2,38 @@ import numpy as np
 import pandas as pd
 import requests
 import json
+import os
 
 from PIL import Image
-# load the image
-image = Image.open('./small-dset/aSqVUgt36gddhmJdI1lXNA.jpg')
-# summarize some details about the image
-print(image.format)
-print(image.mode)
-print(image.size)
-# show the image
-#image.show()
+
+image=[]
+in_image=[]
+out_image=[]
+img_width = 600
+img_height = 400
+
+for i in os.listdir('./small-dset/'):
+    # load the image
+    image.append(Image.open('./small-dset/'+i))
+
+    # summarize some details about the image
+    #print(image.format)
+    #print(image.mode)
+    #print(image.size)
+
+    # show the image
+    #image.show()
+
+    # crop image
+    #width,height=image.size
+    #im1=image.crop(((width-3264)/2,(height-1836)/2,(width-3264)/2+3264,(height-1836)/2+1836))
+    #im1.show()
+
+    # resize image (nearest neighbors) and divide dataset into input and desired output images
+    if i[-3:] == 'jpg':
+        in_image.append(image[-1].resize((img_width,img_height),Image.NEAREST))
+    else:
+        out_image.append(image[-1].resize((img_width,img_height), Image.NEAREST))
+
+
+
