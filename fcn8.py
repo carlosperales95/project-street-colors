1983acd636f257cdbfc3e922ea2763acdaf1926e
EmperorOfPau <carlos@mt2015.com>
2020-01-29 00:37:45 +0100

    trying not to fuck up the branch

--- fcn8.py
+++ fcn8.py
@@ -28,9 +28,11 @@ out_test=[]
 out_valid=[]
 img_width = 600
 img_height = 400
+train_size = 500
+test_size = 100
+valid_size = 100
 
-
-for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/training/images/'):
+for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/training/images/')[0:train_size]:
     # load the image
     in_train.append(Image.open('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/training/images/'+i))
 
@@ -38,7 +40,7 @@ for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapill
     in_train[-1] = np.array(in_train[-1].resize((img_width,img_height),Image.NEAREST))
     in_train[-1] = np.transpose(in_train[-1], (1, 0, 2))
 
-for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/training/labels/'):
+for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/training/labels/')[0:train_size]:
     # load the image
     out_train.append(Image.open('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/training/labels/'+i))
 
@@ -46,7 +48,7 @@ for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapill
     out_train[-1] = np.array(out_train[-1].resize((img_width,img_height), Image.NEAREST))
     out_train[-1] = np.transpose(out_train[-1], (1, 0))
 
-for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/testing/images/'):
+for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/testing/images/')[0:test_size]:
     # load the image
     in_test.append(Image.open('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/testing/images/'+i))
 
@@ -54,7 +56,7 @@ for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapill
     in_test[-1] = np.array(in_test[-1].resize((img_width,img_height),Image.NEAREST))
     in_test[-1] = np.transpose(in_test[-1], (1, 0, 2))
 
-for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/testing/labels/'):
+for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/testing/labels/')[0:test_size]:
     # load the image
     out_test.append(Image.open('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/testing/labels/'+i))
 
@@ -62,7 +64,7 @@ for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapill
     out_test[-1] = np.array(out_test[-1].resize((img_width,img_height), Image.NEAREST))
     out_test[-1] = np.transpose(out_test[-1], (1, 0))
 
-for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/validation/images/'):
+for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/validation/images/')[0:valid_size]:
     # load the image
     in_valid.append(Image.open('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/validation/images/'+i))
 
@@ -71,7 +73,7 @@ for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapill
     in_valid[-1] = np.transpose(in_valid[-1], (1, 0, 2))
 
 
-for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/validation/labels/'):
+for i in os.listdir('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/validation/labels/')[0:valid_size]:
     # load the image
     out_valid.append(Image.open('C:/Users/Leonardo/Documents/Leonardo-Poco importante/mapillarydb/validation/labels/'+i))
 
@@ -116,15 +118,16 @@ model = Model(inputs=images, outputs=predictions)
 
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
-in_train, in_test, out_train, out_test = sklearn.model_selection.train_test_split(in_image,out_image,test_size=0.2)
+#in_train, in_test, out_train, out_test = sklearn.model_selection.train_test_split(in_image,out_image,test_size=0.2)
 
 in_train = np.array([x for x in in_train])
 out_train = np.array([x for x in out_train])
 in_test = np.array([x for x in in_test])
 out_test = np.array([x for x in out_test])
+in_valid = np.array([x for x in in_valid])
+out_valid = np.array([x for x in out_valid])
 
-
-modelzero = model.fit(in_train, out_train, epochs=20, batch_size=3, validation_data=(in_test, out_test))
+modelzero = model.fit(in_train, out_train, epochs=5, batch_size=2, validation_data=(in_valid, out_valid))
 
 plt.plot(modelzero.history['accuracy'], label='accuracy')
 plt.plot(modelzero.history['val_accuracy'], label = 'val_accuracy')
